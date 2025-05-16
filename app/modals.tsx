b2fb65480342a9be84b92544bc435d5b8a31e039
CitalKishan <kishan@cital.co>
2025-05-16 23:59:16 +0530

    * app/modals.tsx
    Removed button and state management for Modal 1, simplifying the Modals component and updating the displayed text.

--- app/modals.tsx
+++ app/modals.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import Modal1 from "../modals/modal1";
+import { Text, View } from "react-native";
 
 export default function Modals() {
-  const [isModal1Visible, setIsModal1Visible] = useState(false);
-
   return (
     <View
       style={{
@@ -13,26 +9,7 @@ export default function Modals() {
         alignItems: "center",
       }}
     >
-      <Text>This is the Modals tab.</Text>
-      <TouchableOpacity
-        style={{
-          marginTop: 20,
-          backgroundColor: "#007AFF",
-          paddingVertical: 12,
-          paddingHorizontal: 24,
-          borderRadius: 8,
-        }}
-        onPress={() => {
-          setIsModal1Visible(true);
-        }}
-      >
-        <Text style={{ color: "white", fontWeight: "500" }}>Open Modal 1</Text>
-      </TouchableOpacity>
-
-      <Modal1
-        visible={isModal1Visible}
-        onRequestClose={() => setIsModal1Visible(false)}
-      />
+      <Text>I'm modal screen</Text>
     </View>
   );
 }
