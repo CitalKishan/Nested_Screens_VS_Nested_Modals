4794eeae9a73b25727f651a365b7ea5bd0a14885
CitalKishan <kishan@cital.co>
2025-05-16 22:37:10 +0530

    * package.json
    Updated start script to clear cache on start.
    
    app/_layout.tsx
    
    Implemented tab navigation with two screens: 'Screens' and 'Modals' using Ionicons for icons.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Screens",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="layers-outline" size={size} color={color} />
+          ),
+          title: "Screens",
+        }}
+      />
+      <Tabs.Screen
+        name="modals"
+        options={{
+          tabBarLabel: "Modals",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="copy-outline" size={size} color={color} />
+          ),
+          title: "Modals",
+        }}
+      />
+    </Tabs>
+  );
 }
